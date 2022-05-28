a58549a6f5acf2375a1c813b834183bbd6726e05
mferrell16 <44271033+mferrell16@users.noreply.github.com>
2022-05-27 22:50:13 -0400

    Create ctf.py

---
+++ ctf.py
@@ -0,0 +1,24 @@
+
+import itertools
+import string
+import zlib
+
+keyspace = string.digits + string.ascii_lowercase
+Correct = zlib.crc32(b'the_intended_answer')
+print(Correct)
+#Try all of character 
+# (or `itertools.product(keyspace, repeat=5)`)
+i = 1
+while True: 
+	print("Testing combinations of length", i)
+	for combination in itertools.product(*[keyspace] * i):  
+	    p = ''.join(combination)
+	    starter = "Black Cybersecurity Association"
+	    s = starter+p
+	    print("Trying: ", s)
+	    testTry = zlib.crc32(s.encode("utf-8"))
+	    if testTry == Correct:
+	    	print("Answer found: ", s)
+	    	exit()
+	print("Not found for length", i)
+	i = i +1 
